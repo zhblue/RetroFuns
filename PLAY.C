b88aaed12dd6fcf364605bacfe315eda190acb06
张浩斌 <newsclan@gmail.com>
2022-02-12 11:41:35 +0800

    play  vgm/vgz files using sbvgm opl2lpt
    tested in dosbox

---
+++ PLAY.C
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <dos.h>
+#include <dir.h>
+#include <conio.h>
+int main(int argn,char ** argv){
+    char dir[1024]=".";
+    char cmd[1024];
+    int i=0;
+    struct ffblk vgms;
+    int next=0;
+    if (argn==2){
+	printf("[%s]\n",argv[1]);
+	sscanf(argv[1],"%s",dir);
+	sprintf(dir+strlen(dir),"\\*.vgz");
+	printf("scan files : %s\n",dir);
+	next=findfirst(dir,&vgms,0);
+	if(next){
+	    sscanf(argv[1],"%s",dir);
+	    sprintf(dir+strlen(dir),"\\*.vgm");
+	    printf("scan files : %s\n",dir);
+	    next=findfirst(dir,&vgms,0);
+	}
+
+	while(!next){
+	    printf("find %s ... \n",vgms.ff_name);
+	    next=findnext(&vgms);
+	    printf("play %s ...\n",vgms.ff_name);
+	    sprintf(cmd,"sbvgm -x -noscan -opl2lpt378 %s\\%s",argv[1],vgms.ff_name);
+	    system(cmd);
+	    sleep(1);
+	    if(kbhit()){
+	       if(getch()==27){
+		   printf("Double Esc pressed ... Exit !\n");
+		   break;
+	       }
+	    }
+	}
+    }
+    printf("Thank you for using OPL2LPT SBVGM player !\n");
+    printf("      https://github.com/zhblue/RetroFuns");
+    sleep(1);
+    return 0;
+}
