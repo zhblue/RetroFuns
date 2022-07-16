494dab32ec9762f4380257b1c93c0af73c46429d
张浩斌 <newsclan@gmail.com>
2022-07-16 10:50:41 +0800

    Update playvgm.c

--- playvgm.c
+++ playvgm.c
@@ -128,6 +128,7 @@
 #include <conio.h>
 #include <ctype.h>
 #include <dos.h>
+#include <dir.h>
 
 #define KEYON	 0x20     /* key-on bit in regs b0 - b8 */
 
@@ -341,7 +342,7 @@ unsigned long ld_int(unsigned char * p){
 }
 void OPL3write(char * raw){
 	int show=0;
- if (show)	printf("%02X %02X %02X =>",raw[0]&0xff,raw[1]&0xff,raw[2]&0xff);
+ if (show)	printf("%02X %02X %02X \n",raw[0]&0xff,raw[1]&0xff,raw[2]&0xff);
    
 	
 	if(raw[0]){
@@ -491,7 +492,63 @@ void playFile(const char * vgmfile){
 		}
 		fclose(vgm);
 }
+int isVgmFile(const char fname[])
+{
+	int l = strlen(fname);
+	if (l <= 4 || stricmp(fname + l - 4, ".vgm") != 0)
+		return 0;
+	else
+		return l - 3;
+}
+int lastIndexOf(char * orig,char * target){
+   int ret=-1;
+   int i;
+   for(i=strlen(orig);i>0;i--){
+      ret=strstr(orig+i,target);
+      if(ret>=0) return ret;
+   }
+   return -1;
+}
+	
+int playdir(int n,char ** argv)
+{
+	    char dir[1024]=".";
+	    char cmd[1024];
+	    char todo[1024]="sbvgm -x -noscan -opl2lpt378";
+	    int tp=0,i=0;
+	    struct ffblk vgms;
+	    int next=0;
+	    next=findfirst(dir,&vgms,0);
+	if(next){
+	    sscanf(argv[1],"%s",dir);
+	    sprintf(dir+strlen(dir),"\\*.vgm");
+	    printf("scan files : %s\n",dir);
+	    next=findfirst(dir,&vgms,0);
+	}
 
+	while(!next){
+	    printf("do task for %s ... \n",vgms.ff_name);
+	    printf("Press twice ESC to QUIT ...\n");
+	    if(argv[1][0]=='*' || (argv[1][0]=='.'&& argv[1][1]=='\\'))
+		sprintf(cmd,"%s",vgms.ff_name);
+	    else
+		sprintf(cmd,"%s\\%s",argv[1],vgms.ff_name);
+	    printf("%s\n",cmd);
+	    playFile(cmd);
+	    sleep(1);
+	    if(kbhit()){
+	       if(getch()==27){
+		   printf("Double Esc pressed ... Exit !\n");
+		   break;
+	       }
+	    }
+	    next=findnext(&vgms);
+	}
+    reset();
+    printf("OPL3 usb serial drive test. \n");
+  
+    return 0;
+}
 void main(int argn,char * argv[])
 {
    int i,val1,val2;
@@ -524,9 +581,12 @@ void main(int argn,char * argv[])
 #else
    fm(0xC0,               1);     /* parallel connection */
 #endif
-   if (argn>1)
-	playFile(argv[1]);
-   else
-	playFile("ultima.vgm");
-   printf("Usage: %s xxx.vgm\n",argv[0]);
+  if(argn>1){
+    if(isVgmFile(argv[1])) playFile(argv[1]);	
+    else playdir(argn,argv);
+  }else{
+     argn=2;
+     argv[1]=".";
+     playdir(argn,argv);
+  }
 }
