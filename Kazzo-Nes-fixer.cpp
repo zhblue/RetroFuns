775ab1f6350a4ca69de9baace59e61dcf4436e0e
张浩斌 <newsclan@gmail.com>
2023-01-24 08:14:18 +0800

    Update Kazzo-Nes-fixer.cpp

--- Kazzo-Nes-fixer.cpp
+++ Kazzo-Nes-fixer.cpp
@@ -4,13 +4,8 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <unistd.h>
-
+#include <dirent.h>
 /*
-这个程序修复由于CHR-ROM不是2的整数次幂而导致无法用kazzo烧写的问题。
-
-
-
-
 .NES文件为模拟用来储存NES卡带的映像。下面是一个.NES文件的结构。
 偏移  字节数  内容
 0－3  4       字符串“NES^Z”用来识别.NES文件
@@ -28,6 +23,21 @@
 -EOF  8KxN  VROM段, 升序排列
 
 
+
+
+SB2000 YX027游戏盘游戏存放地址
+CHRROM 1: 19000-38FFF
+CHRROM 2: 39000-58FFF
+MENU CHR: 59000-59FFF
+MENU CHR: 5A000-5AFFF
+PRGROM 1: B5000-D4FFF
+PRGROM 2: D5000-F4FFF
+
+mapper0：
+    game1 chr：19400-393FF prg:B4800-D47FF,
+    game2 chr: 39400-593FF prg:D4800-F47FF
+my:
+    game2 chr:39400h-D87FFh   prg:EC800h - E87FFh
 */
 // mapper0.ima
 /*
@@ -50,11 +60,167 @@
 
 // mapper4 based
 
+#define BASE_IMA "008.ima"
+
+#define PRG1_START 0x29c00    //米老鼠 32k+32k mapper3               menu2
+#define PRG1_END 0x31bff      //
+#define CHR1_START 0x59c00    
+#define CHR1_END 0x61bff
+
+
+#define PRG2_START 0x39C00   //唐老鸭128k+0k  mapper2                 menu1
+#define PRG2_END 0x59BFF   
+#define CHR2_START 0x00
+#define CHR2_END 0x00
+
+#define PRG3_START 0x35c00  // 有两处一样的   //雷鸟号16k+8k  mapper0   35c00-39bff    6dc00-71bff     menu3
+#define PRG3_END 0x39bff                            //                  39bff
+#define CHR3_START 0x69c10          //0x69c10    0x31c10 
+#define CHR3_END 0x6bbff		     //0x6bbff    0x33bff
+
+
+#define IMA_LEN 737280
+//#define NES_LEN (40976)
 
 #define NES_HEADER_LEN 16
 
 #define PRGSIZE (16<<10)
 #define CHRSIZE (8<<10)
+void fix(char * file1){
+	
+	struct stat s1;
+	int fd;
+	fd = open(file1, 1);
+	fstat(fd,&s1);
+	int nes1len=s1.st_size;
+	//printf("nes1len:%d\n",nes1len);
+	close(fd);
+	
+	FILE * f1=fopen(file1,"rb");
+	
+	
+	unsigned char * nes1=(unsigned char * )malloc(nes1len);
+	fread(nes1,1,nes1len,f1);
+	fclose(f1);
+	
+	int PRGNUM1=nes1[4];
+	int CHRNUM1=nes1[5];
+	int mapper1=nes1[7]>>4;
+	
+	printf("PRGNUM:%d\tCHRNUM:%d\n",PRGNUM1,CHRNUM1);	
+	int upPRG=PRGNUM1;
+	int upCHR=CHRNUM1;
+	for(int i=1;i<10;i++){
+		if(1<<i >= PRGNUM1){
+			upPRG=1<<i;
+			break;
+		} 
+	}
+	if(CHRNUM1>0){
+		for(int i=1;i<10;i++){
+			if(1<<i >= CHRNUM1){
+				upCHR=1<<i;
+				break;
+			} 
+		}
+	}
+	
+	printf("upPRG:%d\tupCHR:%d\n",upPRG,upCHR);	
+	
+	
+	printf("%s:\n",file1);
+	printf("16k  ROM num :%d\t",PRGNUM1);
+	printf(" 8k VROM num :%d\t",CHRNUM1);
+	printf("  mapper num :%d\t",mapper1);
+	printf("nes1len:%d\n",nes1len);
+	char outfile[64];
+	int newNESLEN=nes1len+(upPRG-PRGNUM1)*PRGSIZE+(upCHR-CHRNUM1)*CHRSIZE;
+	
+	if(upPRG==PRGNUM1 && upCHR==CHRNUM1) {
+		printf("PRG-CHR-ROM无需修复!\n");
+		//gets(outfile);
+		return;
+	}else{
+		f1=fopen(file1,"rb");
+		nes1=(unsigned char * )malloc(newNESLEN);
+		fread(nes1,1,nes1len,f1);
+		fclose(f1);	
+	}
+	
+	sprintf(outfile,"%s-fixed.nes",file1);
+	printf("processing ... %s\n",outfile);
+	
+	// |header 16 bytes|PRG PRGNUM1*PRGSIZE| CHR CHRNUM1*CHRSIZE |
+	// |header 16 bytes|PRG PRGNUM1*PRGSIZE| REPEAT-PRG PRGSIZE*(upPRG-PRGNUM1) | CHR CHRNUM1*CHRSIZE| REPEAT-CHR CHRSIZE*(upCHR-CHRNUM1)
+	
+	//int CHRREPEAT_START=nes1len-CHRSIZE*(upCHR-CHRNUM1);
+	int CHRREPEAT_LENGTH=CHRSIZE*(upCHR-CHRNUM1);
+	//REPEAT-CHR
+	for(int i=newNESLEN-1;i>newNESLEN-CHRREPEAT_LENGTH;i--){
+		nes1[i]=nes1[nes1len-(newNESLEN-i)];
+	}
+	//oldchr
+	int old_char_start=newNESLEN-CHRREPEAT_LENGTH-CHRNUM1*CHRSIZE;
+	for(int i=newNESLEN-CHRREPEAT_LENGTH;i>old_char_start;i--){
+		nes1[i]=nes1[nes1len-(newNESLEN-CHRREPEAT_LENGTH-i)];
+	}
+	//REPEAT-PRG
+	int repeat_prg_start=old_char_start-PRGSIZE*(upPRG-PRGNUM1);
+	for(int i=old_char_start;i>repeat_prg_start;i--){
+		nes1[i]=nes1[nes1len-CHRNUM1*CHRSIZE-(old_char_start-i)];
+	}
+	
+	FILE * fp=fopen(outfile,"wb");
+	
+	nes1[4]=upPRG;
+	nes1[5]=upCHR;
+	
+	fwrite(nes1,1,newNESLEN,fp);
+	
+	fclose(fp);
+
+}
+int isNESFile(const char fname[])
+{
+	int l = strlen(fname);
+	if (l <= 4 || stricmp(fname + l - 4, ".nes") != 0)
+		return 0;
+	else
+		return l - 3;
+}
+
+void all(){
+	char vgmfile[1024]=".";	
+	
+	DIR *pdir;
+	struct dirent *pent;
+	if(strlen(vgmfile)==0){
+			strcpy(vgmfile,".");
+		}
+	pdir=opendir(vgmfile); //opendir for playlist
+	if (!pdir){
+		printf ("opendir() failure; terminating");
+		exit(1);
+	}
+	
+	while ((pent=readdir(pdir))){
+		
+		if(isNESFile(pent->d_name)){
+			char filepath[1024];
+			sprintf(filepath,"%s\\%s",vgmfile,pent->d_name);
+			fix(filepath);
+		}
+	}
+	if (errno){
+		printf ("readdir() failure; terminating");
+		exit(1);
+	}
+	closedir(pdir);
+
+	
+	
+}
+
 
 int main(int argc,char* args[])
 {
@@ -62,71 +228,22 @@ int main(int argc,char* args[])
     char file1[60];
     strcpy (file1, "double.nes");
     
-	if (argc>=2) strcpy(file1,args[1]);
-    
+	
 	if(argc==1){
 
-        printf("Usage: %s rom1.nes \n",args[0]);
-        gets(file1);
+        printf("sigle file Usage: %s rom1.nes \n",args[0]);
+        all();
         return(0);
 
-    }
-    struct stat s1;
-    int fd;
-    fd = open(file1, 1);
-    fstat(fd,&s1);
-    int nes1len=s1.st_size;
-    //printf("nes1len:%d\n",nes1len);
-    close(fd);
-   
-	FILE * f1=fopen(file1,"rb");
-  
- 
-    unsigned char * nes1=(unsigned char * )malloc(nes1len);
-    
-    fread(nes1,1,nes1len,f1);
-    
-    int PRGNUM1=nes1[4];
-    int CHRNUM1=nes1[5];
-    int mapper1=nes1[7]>>4;
-  
-    printf("PRGNUM:%d\tCHRNUM:%d\n",PRGNUM1,CHRNUM1);	
-	int uplen=CHRNUM1;
-	for(int i=1;i<10;i++){
-		if(1<<i >= CHRNUM1){
-			uplen=1<<i;
-			break;
-		} 
+    }else if (argc>=2){
+		
+		for(int k=1;k<argc;k++){
+			strcpy(file1,args[k]);
+			fix(file1);
+			
+		}
+		
 	}
-
-	printf("UPLEN:%d\n",uplen);	
-
-	
-    printf("%s:\n",file1);
-    printf("16k  ROM num :%d\t",PRGNUM1);
-    printf(" 8k VROM num :%d\t",CHRNUM1);
-    printf("  mapper num :%d\t",mapper1);
-    printf("nes1len:%d\n",nes1len);
-	int REPEAT_START=nes1len-CHRSIZE*(uplen-CHRNUM1);
-	int REPEAT_LENGTH=CHRSIZE*(uplen-CHRNUM1);
-	char outfile[64];
-	
-    if(uplen==CHRNUM1) {
-		printf("CHR-ROM无需修复!\n");
-		gets(outfile);
-		return 1;
-	}
-
-    sprintf(outfile,"%s-fixed.nes",file1);
-    printf("processing ... %s\n",outfile);
-    FILE * fp=fopen(outfile,"wb");
-	
-	nes1[5]=uplen;
-    fwrite(nes1,1,nes1len,fp);
-	
-	fwrite(&nes1[REPEAT_START],1,REPEAT_LENGTH,fp);
-	
-    fclose(fp);
 
     return 0;
 }
