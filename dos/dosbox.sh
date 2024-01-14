ac5eebdf2cd6b6fa458d951e1c93f05c3219c506
张浩斌 <newsclan@gmail.com>
2024-01-14 20:17:36 +0800

    Create dosbox.sh

---
+++ dos/dosbox.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir dosbox
+cd dosbox
+wget https://vieju.net/pcdosmini/download/PCDosMini_v1.zip
+unzip PCDosMini_v1.zip
+apt-get install dosbox
+cat >> ~/.dosbox/*.conf << EOF
+mount c: ~/dosbox
+c:
+dir /w
+EOF
